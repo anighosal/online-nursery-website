1d1040ec19500cdeb7f8e879dd245894d15411b9
anighosal <aasgfamily@gmail.com>
2025-04-08 18:27:02 +0600

    image updated

--- src/pages/ImageGallery.tsx
+++ src/pages/ImageGallery.tsx
@@ -1,98 +1,34 @@
 import Container from "@/components/ui/Container";
-import Gallery from "react-photo-gallery";
 
 const photos = [
-  {
-    src: "https://i.ibb.co/HpdN1tQ/istockphoto-177320705-612x612.jpg",
-    width: 1,
-    height: 1,
-  },
-  {
-    src: "https://i.ibb.co/TqfqpHq/images-3.jpg",
-    width: 1,
-    height: 1,
-  },
-  {
-    src: "https://i.ibb.co/HzYmjsm/images-2.jpg",
-    width: 1,
-    height: 1,
-  },
-  {
-    src: "https://i.ibb.co/nMZfhCS/snake-plant.jpg",
-    width: 1,
-    height: 1,
-  },
-  {
-    src: "https://i.ibb.co/7jWRwLf/713b6h-Lg-IDL-AC-UF1000-1000-QL80.jpg",
-    width: 1,
-    height: 1,
-  },
-  {
-    src: "https://i.ibb.co/mScFsjf/images-1.jpg",
-    width: 1,
-    height: 1,
-  },
-  {
-    src: "https://i.ibb.co/tMp00rs/images.jpg",
-    width: 1,
-    height: 1,
-  },
-  {
-    src: "https://i.ibb.co/MR4zq4C/apples-2788599-1280.jpg",
-    width: 1,
-    height: 1,
-  },
-  {
-    src: "https://i.ibb.co/ky8MkVj/oranges-1117628-640.jpg",
-    width: 1,
-    height: 1,
-  },
-  {
-    src: "https://i.ibb.co/Nsv0bh3/istockphoto-157193938-612x612.jpg",
-    width: 1,
-    height: 1,
-  },
-  {
-    src: "https://i.ibb.co/thzWzpS/b9f74faae7f2c1b3865e007116cfb8fb.jpg",
-    width: 1,
-    height: 1,
-  },
-  {
-    src: "https://i.ibb.co/thzWzpS/b9f74faae7f2c1b3865e007116cfb8fb.jpg",
-    width: 1,
-    height: 1,
-  },
-  {
-    src: "https://i.ibb.co/thzWzpS/b9f74faae7f2c1b3865e007116cfb8fb.jpg",
-    width: 1,
-    height: 1,
-  },
-  {
-    src: "https://i.ibb.co/thzWzpS/b9f74faae7f2c1b3865e007116cfb8fb.jpg",
-    width: 1,
-    height: 1,
-  },
+  { src: "https://i.ibb.co/HpdN1tQ/istockphoto-177320705-612x612.jpg" },
+  { src: "https://i.ibb.co/TqfqpHq/images-3.jpg" },
+  { src: "https://i.ibb.co/HzYmjsm/images-2.jpg" },
+  { src: "https://i.ibb.co/nMZfhCS/snake-plant.jpg" },
+  { src: "https://i.ibb.co/7jWRwLf/713b6h-Lg-IDL-AC-UF1000-1000-QL80.jpg" },
+  { src: "https://i.ibb.co/mScFsjf/images-1.jpg" },
+  { src: "https://i.ibb.co/tMp00rs/images.jpg" },
+  { src: "https://i.ibb.co/MR4zq4C/apples-2788599-1280.jpg" },
+  { src: "https://i.ibb.co/ky8MkVj/oranges-1117628-640.jpg" },
+  { src: "https://i.ibb.co/Nsv0bh3/istockphoto-157193938-612x612.jpg" },
+  { src: "https://i.ibb.co/thzWzpS/b9f74faae7f2c1b3865e007116cfb8fb.jpg" },
+  { src: "https://i.ibb.co/thzWzpS/b9f74faae7f2c1b3865e007116cfb8fb.jpg" },
 ];
-
-const CustomImage = ({ index, photo, onClick }) => {
-  return (
-    <div className=" mx-auto">
-      <img
-        src={photo.src}
-        alt={`Image ${index}`}
-        className="w-[250px] h-[250px] object-cover rounded-lg shadow-md"
-        onClick={(e) => onClick && onClick(e, { index, photo })}
-      />
-    </div>
-  );
-};
 
 const ImageGallery = () => {
   return (
     <Container>
       <h2 className="text-2xl font-bold text-center mb-4">Image Gallery</h2>
-      <div className="flex justify-center">
-        <Gallery photos={photos} renderImage={CustomImage} />
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+        {photos.map((photo, index) => (
+          <div key={index} className="w-full h-[250px]">
+            <img
+              src={photo.src}
+              alt={`Image ${index}`}
+              className="w-full h-full object-cover rounded-lg shadow-md"
+            />
+          </div>
+        ))}
       </div>
     </Container>
   );
